50f9487e4bb44ae8722b4c6c0f74769ac8892491
GabsLopes055 <lopesgabriel055@gmail.com>
2024-11-15 15:26:44 -0300

    melhorando a versao mobile

--- src/app/private/admin/pages/colaboradores/components/editar-colaborador/editar-colaborador.component.ts
+++ src/app/private/admin/pages/colaboradores/components/editar-colaborador/editar-colaborador.component.ts
@@ -15,10 +15,15 @@ import { RadioComponent } from "../../../../../../shared/radio/radio.component";
   styleUrl: './editar-colaborador.component.scss',
 })
 export class EditarColaboradorComponent {
+
+  isProfessor: boolean = false;
+  campoObrigatorio: boolean = false;
+
   form = new FormGroup({
-    nome: new FormControl('Maria da Silva', Validators.required),
-    email: new FormControl('mariasilva@ayrtonsenna.gov', Validators.required),
-    materia: new FormControl('Português', Validators.required),
+    nome: new FormControl('Nome do Colaborador', Validators.required),
+    email: new FormControl('E-mail.colaborador.com.br', Validators.required),
+    materia: new FormControl('Materia', Validators.required),
+    tipo: new FormControl('professor', Validators.required),
   });
 
   constructor(
@@ -28,6 +33,14 @@ export class EditarColaboradorComponent {
 
   receberEvento(event: boolean) {
     console.log(event)
+  }
+
+  changeValue(event: any) {
+    if(event === 'professor') {
+      this.isProfessor = true;
+    } else {
+      this.isProfessor = false;
+    }
   }
 
   salvar() {
