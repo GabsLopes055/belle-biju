2f55200498afba728c6e837def71c9d1ee100466
Gabs2305 <lopesgabriel055@gmail.com>
2024-11-30 11:59:53 -0300

    Finalizado a tela de turmas

--- src/app/private/admin/pages/turmas/components/visualizar-turma/components/aba-aluno/vincular-aluno/vincular-aluno.component.ts
+++ src/app/private/admin/pages/turmas/components/visualizar-turma/components/aba-aluno/vincular-aluno/vincular-aluno.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ModalComponent } from "../../../../../../../../../shared/modal/modal.component";
 import { DividerComponent } from "../../../../../../../../../shared/divider/divider.component";
 import { InputIconComponent } from "../../../../../../../../../shared/input-icon/input-icon.component";
 import { CheckboxComponent } from "../../../../../../../../../shared/checkbox/checkbox.component";
 import { ButtonComponent } from "../../../../../../../../../shared/button/button.component";
 import { ModalService } from '../../../../../../../../../shared/modal/modal.service';
+import { AlunosService } from '../../../../../../alunos/alunos.service';
+import { alunoResponse, listaAlunosResponse } from '../../../../../../../../../models/alunos.interface';
+import { filtroDeBusca } from '../../../../../../../../../models/filtro-busca.interface';
 
 @Component({
   selector: 'app-vincular-aluno',
@@ -13,12 +16,32 @@ import { ModalService } from '../../../../../../../../../shared/modal/modal.serv
   templateUrl: './vincular-aluno.component.html',
   styleUrl: './vincular-aluno.component.scss'
 })
-export class VincularAlunoComponent {
+export class VincularAlunoComponent implements OnInit {
+  filtroBuscarAlunos: filtroDeBusca = {
+    limit: 100,
+    page: 1
+  }
+  alunos: alunoResponse[] = [];
 
   constructor(
-    private readonly modalService: ModalService
+    private readonly modalService: ModalService,
+    private readonly alunosService: AlunosService
   ){}
 
+  ngOnInit(): void {
+    this.buscarTodosAlunos();
+  }
+
+  buscarTodosAlunos() {
+    this.alunosService.listarAlunos(this.filtroBuscarAlunos).subscribe({
+      next: (alunos) => {
+        this.alunos = alunos.data
+      },
+      error: (err) => {
+
+      },
+    })
+  }
 
   vincular() {
     this.modalService.close();
--- src/app/private/admin/pages/turmas/components/visualizar-turma/components/aba-dados-turma/dados-turma/dados-turma.component.ts
+++ src/app/private/admin/pages/turmas/components/visualizar-turma/components/aba-dados-turma/dados-turma/dados-turma.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { TurmasService } from '../../../../../turmas.service';
+import { ButtonComponent } from "../../../../../../../../../shared/button/button.component";
 
 @Component({
   selector: 'dados-turma',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './dados-turma.component.html',
   styleUrl: './dados-turma.component.scss'
 })
 export class DadosTurmaComponent {
 
+
+  constructor(private readonly turmaService: TurmasService) {}
+
+
+  editar() {
+    this.turmaService.steps.next('editar-turma');
+  }
 }
