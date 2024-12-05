ffd48afa3608505d2c0bb7fa29c584f292ff2b67
Gabs2305 <lopesgabriel055@gmail.com>
2024-12-04 23:55:19 -0300

    Realizado as alterações da tela de turmas

--- src/app/private/admin/pages/turmas/components/visualizar-turma/components/aba-notas/lancar-nota/lancar-nota.component.ts
+++ src/app/private/admin/pages/turmas/components/visualizar-turma/components/aba-notas/lancar-nota/lancar-nota.component.ts
@@ -1,27 +1,69 @@
 import { Component } from '@angular/core';
 import { ModalService } from '../../../../../../../../../shared/modal/modal.service';
-import { ButtonComponent } from "../../../../../../../../../shared/button/button.component";
-import { ModalComponent } from "../../../../../../../../../shared/modal/modal.component";
+import { ButtonComponent } from '../../../../../../../../../shared/button/button.component';
+import { ModalComponent } from '../../../../../../../../../shared/modal/modal.component';
 import { LancarNotaAlunoComponent } from './components/lancar-nota-aluno/lancar-nota-aluno.component';
+import {
+  Tab,
+  TabsComponent,
+} from '../../../../../../../../../shared/tabs/tabs.component';
 
 @Component({
   selector: 'lancar-nota',
   standalone: true,
-  imports: [ButtonComponent, ModalComponent],
+  imports: [ButtonComponent, ModalComponent, TabsComponent],
   templateUrl: './lancar-nota.component.html',
-  styleUrl: './lancar-nota.component.scss'
+  styleUrl: './lancar-nota.component.scss',
 })
 export class LancarNotaComponent {
-  constructor(
-    private readonly modalService: ModalService
-  ){}
+
+  opcaoTabSelecionada = '';
+
+  tabs: Tab[] = [
+    {
+      icon: '',
+      label: '1º Bimestre',
+      value: 'primeiro-bimestre',
+      selected: false,
+    },
+    {
+      icon: '',
+      label: '2º Bimestre',
+      value: 'segundo-bimestre',
+      selected: false,
+    },
+    {
+      icon: '',
+      label: '3º Bimestre',
+      value: 'terceiro-bimestre',
+      selected: false,
+    },
+    {
+      icon: '',
+      label: '4º Bimestre',
+      value: 'quarto-bimestre',
+      selected: false,
+    },
+    {
+      icon: '',
+      label: 'Geral',
+      value: 'geral',
+      selected: false,
+    },
+  ];
+
+  constructor(private readonly modalService: ModalService) {}
 
   editarNota() {
     // this.modalService.open(EditarAdvertenciaComponent)
   }
 
   abrirModalLancarNota() {
-    this.modalService.open(LancarNotaAlunoComponent)
+    this.modalService.open(LancarNotaAlunoComponent);
+  }
+
+  chosenTab(tab: string) {
+    this.opcaoTabSelecionada = tab;
   }
 
   fechar() {
